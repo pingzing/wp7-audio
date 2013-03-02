0bf2b055a3ea66b3e0d2392e790e0405fa2a3365
NEIL MCALISTER <mcalistern@gmail.com>
2013-03-01 17:38:44 -0800

    Finished Recording method. Added HeaderWrite and HeaderUpdate functions. Expanded SavedAudio model. Began early versions of saving to the phone.

--- AudioRecorder/AudioRecorder/Models/SavedAudio.cs
+++ AudioRecorder/AudioRecorder/Models/SavedAudio.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,11 +12,92 @@ using System.Windows.Shapes;
 
 namespace AudioRecorder
 {
-    public class SavedAudio
+    public class SavedAudio : INotifyPropertyChanged
     {
-        public int FileSize { get; set; }
-        public string FileName { get; set; }
-        public string Description { get; set; }
+        private int fileSize;
+        public int FileSize
+        {
+            get { return this.fileSize; }
+            set
+            {
+                if (this.fileSize != value)
+                {
+                    this.fileSize = value;
+                    OnPropertyChanged("FileSize");
+                }
+            }
+        }
+
+        private string fileName;
+        public string FileName 
+        {
+            get { return this.fileName; }
+            set
+            {
+                if (this.fileName != value)
+                {
+                    this.fileName = value;
+                    OnPropertyChanged("FileName");
+                }
+            }
+        }
+
+        private string description;
+        public string Description 
+        {
+            get { return this.description; }
+            set
+            {
+                if (this.description != value)
+                {
+                    this.description = value;
+                    OnPropertyChanged("Description");
+                }
+            }
+        }
+
+        private DateTime timeStamp;
+        public DateTime TimeStamp 
+        {
+            get { return this.timeStamp; }
+            set
+            {
+                if (this.timeStamp != value)
+                {
+                    this.timeStamp = value;
+                    OnPropertyChanged("TimeStamp");
+                }
+            }
+        }
+
+        private string filePath;
+        public string FilePath 
+        {
+            get { return this.filePath; }
+            set
+           {
+               if (this.filePath != value)
+               {
+                   this.filePath = value;
+                   OnPropertyChanged("FilePath");
+               }
+            }
+        }
+
+        private string sourcePath;
+        public string SourcePath 
+        {
+            get { return this.sourcePath; }
+            set
+            {
+                if (this.sourcePath != value)
+                {
+                    this.sourcePath = value;
+                    OnPropertyChanged("SourcePath");
+                }
+            }
+        }
+
 
         //Default constructor
         public SavedAudio()
@@ -23,6 +105,25 @@ namespace AudioRecorder
             this.FileSize = 0;
             this.FileName= "Default";
             this.Description = "Blah";
+        }
+
+        public SavedAudio(int fileSize, string fileName, string description, DateTime timeStamp, string filePath, string sourcePath)
+        {
+            this.fileSize = fileSize;
+            this.fileName = fileName;
+            this.description = description;
+            this.timeStamp = timeStamp;
+            this.filePath = filePath;
+            this.sourcePath = sourcePath;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
--- AudioRecorder/AudioRecorder/ViewModels/MainPageViewModel.cs
+++ AudioRecorder/AudioRecorder/ViewModels/MainPageViewModel.cs
@@ -23,19 +23,20 @@ namespace AudioRecorder.ViewModels
         private byte[] audioBuffer;        
         bool stopRequested;
         MemoryStream currentRecordingStream;
+        ObservableCollection<SavedAudio> savedAudio = new ObservableCollection<SavedAudio>();
 
         public MainPageViewModel()
         {
-            //Test values. Remove these later
-            ObservableCollection<SavedAudio> temp = new ObservableCollection<SavedAudio>();
+            //Test values. Remove these later            
             SavedAudio file1 = new SavedAudio();
             SavedAudio file2 = new SavedAudio();
-            temp.Add(file1);
-            temp.Add(file2);
-            ListItemsSource = temp;
+            savedAudio.Add(file1);
+            savedAudio.Add(file2);
+            ListItemsSource = savedAudio;
             //End test values
 
             this.recordCommand = new DelegateCommand(this.RecordAction);
+            this.stopCommand = new DelegateCommand(this.RequestStopRecordingAction);
 
         }
 
@@ -71,10 +72,13 @@ namespace AudioRecorder.ViewModels
             }
             this.stopRequested = false;
             this.currentRecordingStream = new MemoryStream(MEMORY_STREAM_SIZE);
+
+            //Write the initial header to the WAVE file. This'll be updated after we finish recording.
+            WriteHeader(this.currentRecordingStream, (int)this.currentMicrophone.SampleRate);
             this.currentMicrophone.Start();
         }
 
-        public void RequestStopRecording()
+        public void RequestStopRecordingAction(object p)
         {
             this.stopRequested = true;
         }
@@ -88,13 +92,86 @@ namespace AudioRecorder.ViewModels
                 return;
             }
             this.currentMicrophone.Stop();
+            UpdateHeader(this.currentRecordingStream);
 
             var isoStore = System.IO.IsolatedStorage.IsolatedStorageFile.GetUserStoreForApplication();
 
-            /*using (var targetFile = isoStore.CreateFile(FileName))
+            using (var targetFile = isoStore.CreateFile("newFile.wav"))
             {
-                //Write to Isolated Storage, update list, create WAVE header, etc etc etc.
-            }*/
+                //Write to Isolated Storage, update list, create WAVE header, etc etc etc.                
+                var dataBuffer = this.currentRecordingStream.GetBuffer();
+                targetFile.Write(dataBuffer, 0, (int)this.currentRecordingStream.Length);                
+                SavedAudio newFile = new SavedAudio((int)targetFile.Length, "newFile", "Loooooooooooooong Description", DateTime.Now, "\newFile.wav", "\\");
+                savedAudio.Add(newFile);
+                targetFile.Flush();
+                targetFile.Close();
+            }
+        }
+
+        public void WriteHeader(Stream stream, int sampleRate)
+        {
+            const int bitsPerSample = 16;
+            const int bytesPerSample = bitsPerSample / 8;
+            var encoding = System.Text.Encoding.UTF8;
+
+            // ChunkID Contains the letters "RIFF" in ASCII form (0x52494646 big-endian form).
+            stream.Write(encoding.GetBytes("RIFF"), 0, 4);
+
+            // NOTE this will be filled in later
+            stream.Write(BitConverter.GetBytes(0), 0, 4);
+
+            // Format Contains the letters "WAVE"(0x57415645 big-endian form).
+            stream.Write(encoding.GetBytes("WAVE"), 0, 4);
+
+            // Subchunk1ID Contains the letters "fmt " (0x666d7420 big-endian form).
+            stream.Write(encoding.GetBytes("fmt "), 0, 4);
+
+            // Subchunk1Size 16 for PCM.  This is the size of the rest of the Subchunk which follows this number.
+            stream.Write(BitConverter.GetBytes(16), 0, 4);
+
+            // AudioFormat PCM = 1 (i.e. Linear quantization) Values other than 1 indicate some form of compression.
+            stream.Write(BitConverter.GetBytes((short)1), 0, 2);
+
+            // NumChannels Mono = 1, Stereo = 2, etc.
+            stream.Write(BitConverter.GetBytes((short)1), 0, 2);
+
+            // SampleRate 8000, 44100, etc.
+            stream.Write(BitConverter.GetBytes(sampleRate), 0, 4);
+
+            // ByteRate =  SampleRate * NumChannels * BitsPerSample/8
+            stream.Write(BitConverter.GetBytes(sampleRate * bytesPerSample), 0, 4);
+
+            // BlockAlign NumChannels * BitsPerSample/8 The number of bytes for one sample including all channels.
+            stream.Write(BitConverter.GetBytes((short)(bytesPerSample)), 0, 2);
+
+            // BitsPerSample    8 bits = 8, 16 bits = 16, etc.
+            stream.Write(BitConverter.GetBytes((short)(bitsPerSample)), 0, 2);
+
+            // Subchunk2ID Contains the letters "data" (0x64617461 big-endian form).
+            stream.Write(encoding.GetBytes("data"), 0, 4);
+
+            // NOTE to be filled in later
+            stream.Write(BitConverter.GetBytes(0), 0, 4);
+        }
+
+        public void UpdateHeader(Stream stream)
+        {
+            if (!stream.CanSeek)
+            {
+                throw new Exception("Cant properly update audio file's header to reflect changes.");
+                var oldPos = stream.Position;
+
+                //ChunkSize  36 + SubChunk2Size
+                stream.Seek(4, SeekOrigin.Begin);
+                stream.Write(BitConverter.GetBytes((int)stream.Length - 8), 0, 4);
+
+                //Subchunk2Size == NumSamples * NumChannels * BitsPerSample/8 This is the number of bytes in the data.
+                stream.Seek(40, SeekOrigin.Begin);
+                stream.Write(BitConverter.GetBytes((int)stream.Length - 44), 0, 4);
+
+                //seek the stream back to the end of the stream.
+                stream.Seek(oldPos, SeekOrigin.Begin);
+            }
         }
 
         private DelegateCommand recordCommand;
@@ -103,6 +180,15 @@ namespace AudioRecorder.ViewModels
             get
             {
                 return this.recordCommand;
+            }
+        }
+
+        private DelegateCommand stopCommand;
+        public DelegateCommand StopCommand
+        {
+            get 
+            {
+                return this.stopCommand;
             }
         }
     }
