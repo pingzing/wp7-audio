17568f30a950feefd0683aef4ac1f1a34637f6ad
NEIL MCALISTER <mcalistern@gmail.com>
2013-07-02 21:27:26 -0700

    Added.

---
+++ AudioRecorder/AudioRecorder/Helpers/TimeSpanToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace AudioRecorder.Helpers
+{
+    public class TimeSpanToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TimeSpan ts = (TimeSpan)value;
+            return ts.ToString(@"hh\:mm\:ss");
+        }
+
+        public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException("Converter can't convert back.");
+        }
+    }
+}
